46d3d77002906240e41a340b9ef743a278508cbb
bluemacaw <ragdollnoone@163.com>
2024-10-10 14:06:01 +0800

    bug fix remove one todo

--- concurrency/src/test/java/io/bluemacaw/concurrency/record/SingletonFactoryTest.java
+++ concurrency/src/test/java/io/bluemacaw/concurrency/record/SingletonFactoryTest.java
@@ -1,17 +1,22 @@
 package io.bluemacaw.concurrency.record;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
+@Slf4j
 public class SingletonFactoryTest {
     @Test
     public void mainTest() throws Exception {
         for (int i = 0; i < 10; i++) {
             Thread t = new Thread(() -> {
                 SingletonFactory instance = SingletonFactory.getInstance();
-                instance.sayReady(); // TODO:没有日志输出
+                instance.sayReady();
             });
 
+            t.start();
             t.join();
         }
+
+        // Thread.currentThread().join();
     }
 }
