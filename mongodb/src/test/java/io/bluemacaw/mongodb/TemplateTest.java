4768afc599975a6a2591ec44ebbeae77f74cb132
zhoushanhong <AraticFox@163.com>
2025-10-20 22:20:20 +0800

    阶段提交

--- mongodb/src/test/java/io/bluemacaw/mongodb/TemplateTest.java
+++ mongodb/src/test/java/io/bluemacaw/mongodb/TemplateTest.java
@@ -1,5 +1,21 @@
 package io.bluemacaw.mongodb;
 
+import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+@Slf4j
 @SpringBootTest
 public class TemplateTest {
+    @Resource
+    MongoTemplate mongoTemplate;
+
+    @Test
+    public void testCreateCollection(){
+        boolean exists = mongoTemplate.collectionExists("books");
+        log.info("collection books is exists: {}", exists);
+    }
+
 }
