a8e216ae72c874f3fb1f11dc3b49bd8874719183
bluemacaw-zhou <arcticfox.zhou@gmail.com>
2025-11-17 16:26:56 +0800

    Create ScheduleConfig.java

---
+++ mongodb/src/main/java/io/bluemacaw/mongodb/config/ScheduleConfig.java
@@ -0,0 +1,12 @@
+package cn.com.wind.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * 定时任务配置
+ */
+@Configuration
+@EnableScheduling
+public class ScheduleConfig {
+}
