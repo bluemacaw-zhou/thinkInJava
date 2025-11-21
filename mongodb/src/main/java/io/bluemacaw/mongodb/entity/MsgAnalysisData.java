7bbdce36e53d02a07c7549d933d1230b1406de35
bluemacaw-zhou <arcticfox.zhou@gmail.com>
2025-11-21 17:16:05 +0800

    Update MsgAnalysisData.java

--- mongodb/src/main/java/io/bluemacaw/mongodb/entity/MsgAnalysisData.java
+++ mongodb/src/main/java/io/bluemacaw/mongodb/entity/MsgAnalysisData.java
@@ -12,11 +12,13 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 public class MsgAnalysisData {
-    private String msgId;
+   private String msgId;
 
     private long fromId;
 
     private long contactId;
+
+    private String sessionId;
 
     private int contactType;
 
@@ -49,4 +51,29 @@ public class MsgAnalysisData {
     private LocalDateTime createTime;
 
     private LocalDateTime updateTime;
+
+    /**
+     * 生成 sessionId
+     * 规则：比较 fromId 和 contactId，小的在前，大的在后，用下划线拼接
+     * 例如：fromId=1001, contactId=2001 → sessionId="1001_2001"
+     *      fromId=5000, contactId=3000 → sessionId="3000_5000"
+     */
+    public void generateSessionId() {
+        if (fromId < contactId) {
+            this.sessionId = fromId + "_" + contactId;
+        } else {
+            this.sessionId = contactId + "_" + fromId;
+        }
+    }
+
+    /**
+     * 静态方法：根据两个ID生成 sessionId
+     */
+    public static String generateSessionId(long id1, long id2) {
+        if (id1 < id2) {
+            return id1 + "_" + id2;
+        } else {
+            return id2 + "_" + id1;
+        }
+    }
 }
